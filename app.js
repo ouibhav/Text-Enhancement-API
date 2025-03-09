314f078d1dda28304b8058f6566e1e700a1458eb
Vibhav Sharma <114804486+ouibhav@users.noreply.github.com>
2025-03-09 23:02:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post('/modify', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
